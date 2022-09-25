36beaf9fd60d660c1f1c48c5bdf1fd3a1e242652
Vedashree20 <79038911+Vedashree20@users.noreply.github.com>
2022-09-25 13:32:45 +0530

    Validation added

--- register.php
+++ register.php
@@ -5,13 +5,103 @@
         $data = htmlspecialchars($data);
         return $data;
     }
+
+    $errName = $errPassword = $errEmail = $errMobile = "";
+    $username = $password = $mobile = $email = "";
+    $errorCheck = TRUE;
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = $_POST['email'];
         $username = $_POST['username'];
         $password = $_POST['password'];
         $mobile = $_POST['mobile'];
+    
+
+    if (empty($email)) {
+        $errEmail = "*Email Required";
+        $errorCheck = FALSE;
+    }else {
+        $email = test_input($email);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errEmail = "*Invalid Email";
+            $errorCheck = FALSE;
+        }
+        if ($errorCheck){
+            $errorCheck = TRUE;
+        }
     }
-      
+
+    if (empty($username)) {
+        $errName = "*Username Required";
+        $errorCheck = FALSE;
+    } else {
+        $username = test_input($username);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$username)) {
+            $errName = "*Only letters and white space allowed";
+            $errorCheck = FALSE;
+        }
+        else{
+            $errorCheck = TRUE;
+        }
+    }
+
+    if (empty($password)){
+        $errPassword = "*Password Required";
+        $errorCheck = FALSE;
+    } else {
+        $password = test_input($password);
+        if(strlen($password) < 8) {
+            $errPassword = "*Password must have 8 characters";
+            $errorCheck = FALSE;
+        }
+        else if (!preg_match("/[0-9]/", $password)) {
+            $errPassword = "*Password should contain a Number";
+            $errorCheck = FALSE;
+        }
+        if ($errorCheck){
+            $errorCheck = TRUE;
+        }
+    }
+
+    if (empty($mobile)){
+        $errMobile = "*Mobile Number Required";
+        $errorCheck = FALSE;
+    }
+    else {
+        $mobile = test_input($mobile);
+
+        if(!preg_match('/^[0-9]{10}+$/', $mobile))
+            {
+                $errMobile = "*Invalid Phone Number";
+                $errorCheck = FALSE;         
+            } 
+            else 
+            {
+                $errorCheck = TRUE;   
+            }
+    }
+    if ($errorCheck)
+        {
+                
+            if (isset($_POST['agree'])): 
+                    {
+                        $message = "Registration Successful!";
+                        echo "<script>alert('$message');
+                                window.location.href='/MiniProject/NoteKeeper/notes.php';
+                            </script>";
+                    }
+            else:
+                {
+                    $message = "You have not agreed to the terms and conditions!";
+                        echo "<script>alert('$message');
+                                window.location.href='/MiniProject/NotekKeeper/register.php';
+                            </script>";
+                }
+
+            endif;
+        }
+                    
+    }      
     
 ?>
 
@@ -50,16 +140,26 @@
     </nav>
 
     <body>
+
+
         <div class="login-page" style="width:500px;">
             <div class="form">
                 <span class="active"> Sign Up </span>
                 <p>Please register your account</p>
                 <form class="login-form" action='register.php' method="POST">
-                    <input name='email' id="email" type="text" placeholder="Email ID"/>
-                    <input id="username" name='username' type="text" placeholder="Set Username" />
+                    <input name='email' id="email" type="text" placeholder="Email ID" value = "<?php echo $email;?>"/>
+                    <p class="error"><?php echo $errEmail;?></p>
+                    <input id="username" name='username' type="text" placeholder="Set Username" value = "<?php echo $username;?>" />
+                    <p class="error"><?php echo $errName;?></p>
                     <input id="password" name='password' type="password" placeholder="Set Password" />
-                    <input id="mobile" name='mobile' type="text" placeholder="Mobile No"/>
-                    <button>Register</button>
+                    <p class="error"><?php echo $errPassword;?></p>
+                    <input id="mobile" name='mobile' type="text" placeholder="Mobile No" value = "<?php echo $mobile;?>"/>
+                    <p class="error"><?php echo $errMobile;?></p>
+                    <br> Agree to Terms of Service:
+                    <input type="checkbox" name="agree">
+                    <br>
+                    <input type="hidden" name="form_submitted" value="1" />
+                    <button type="Submit">Register</button>
                     <p class="message">Already registered? <a href="./login.php">LogIn</a></p>
                 </form>
             </div>
