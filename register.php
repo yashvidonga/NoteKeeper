df3ed47c366b93ea720088514525c8d2a7c7cd1b
Vedashree20 <79038911+Vedashree20@users.noreply.github.com>
2022-11-21 23:37:42 +0530

    Database added and errors fixed.

--- register.php
+++ register.php
@@ -174,8 +174,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <title>Sign Up</title>
-        <link rel="stylesheet" href="./registerstyle.css">
-        //<link rel="stylesheet" type="text/css" href="<?php echo (($_COOKIE['style'] == "dark")?'registerstyle_dark':'registerstyle') ?>.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo (($_COOKIE['style'] == "dark")?'registerstyle_dark':'registerstyle') ?>.css" />
         <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     </head>
     <nav class="navbar navbar-expand-lg navbar-light fixed-top">
